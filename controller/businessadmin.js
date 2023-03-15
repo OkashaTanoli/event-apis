8b91f85933b5f3f8e8b0e22e3883ae500e6076b3
okashatanoli12345@gmail.com <64710012+OkashaTanoli@users.noreply.github.com>
2023-03-15 06:31:09 +0500

    business admin edit delete api created

--- controller/businessadmin.js
+++ controller/businessadmin.js
@@ -84,7 +84,79 @@ const LoginBusinessAdmin = async (req, res) => {
 
 
 
+
+
+const GetBusinessAdmin = async (req, res) => {
+    const adminId = req.userData.adminId
+    try {
+        const businessAdmin = await BusinessAdmin.findOne({ _id: adminId })
+        if (!businessAdmin) {
+            return res.status(401).json({
+                message: 'auth token invalid'
+            })
+        }
+        res.status(200).json(businessAdmin)
+    }
+    catch (err) {
+        res.status(500).json({
+            error: err
+        })
+    }
+}
+
+
+
+const DeleteBusinessAdmin = async (req, res) => {
+    const adminId = req.userData.adminId
+    try {
+        const businessAdmin = await BusinessAdmin.findOneAndDelete({ _id: adminId })
+        if (!businessAdmin) {
+            return res.status(401).json({
+                message: 'auth token invalid'
+            })
+        }
+        res.status(200).json({ message: 'Business admin deleted successfully' })
+    }
+    catch (err) {
+        res.status(500).json({
+            error: err
+        })
+    }
+}
+
+
+
+
+
+const UpdateBusinessAdmin = async (req, res) => {
+    const adminId = req.userData.adminId
+    const newObj = {}
+    for (let i = 0; i < Object.keys(req.body).length; i++) {
+        newObj[Object.keys(req.body)[i]] = Object.values(req.body)[i]
+    }
+    try {
+        const businessAdmin = await BusinessAdmin.findOneAndUpdate({ _id: adminId }, newObj)
+        if (!businessAdmin) {
+            return res.status(401).json({
+                message: 'auth token invalid'
+            })
+        }
+        res.status(200).json({ message: 'Business admin updated successfully' })
+    }
+    catch (err) {
+        res.status(500).json({
+            error: err
+        })
+    }
+}
+
+
+
+
 module.exports = {
     SignUpBusinessAdmin,
-    LoginBusinessAdmin
+    LoginBusinessAdmin,
+    GetBusinessAdmin,
+    UpdateBusinessAdmin,
+    DeleteBusinessAdmin
 }
